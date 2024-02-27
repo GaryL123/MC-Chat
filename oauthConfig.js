ae59a112576b6a393e9f4aaeb9d0ce1cc0514131
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-02-26 22:56:30 -0500

    Update oauthConfig.js

--- oauthConfig.js
+++ oauthConfig.js
@@ -3,17 +3,14 @@ import * as AuthSession from 'expo-auth-session';
 // Construct the authorization URL manually
 const oauthConfig = {
     clientId: '706298541322-0h4ukmt3p2c5qulh9bilms7ti8grnlov.apps.googleusercontent.com',
-    redirectUri: AuthSession.makeRedirectUri({
-        useProxy: true, // Adjust based on your deployment
-        native: 'com.mcchat:/oauth2redirect', // Ensure this matches the scheme configured for your app
-    }),
+    redirectUri: 'https://mc-chat-b6bef.firebaseapp.com/__/auth/handler', // Directly using Firebase redirect URI
     scopes: ['openid', 'profile', 'email'],
 };
 
 // Helper function to construct the auth URL
 function createAuthUrl(oauthConfig) {
     const scope = encodeURIComponent(oauthConfig.scopes.join(' '));
-    const redirectUri = encodeURIComponent(oauthConfig.redirectUri);
+    const redirectUri = oauthConfig.redirectUri;
     const clientId = encodeURIComponent(oauthConfig.clientId);
     return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${scope}`;
 }
@@ -21,7 +18,7 @@ function createAuthUrl(oauthConfig) {
 export const obtainGoogleAccessToken = async () => {
     try {
         const authUrl = createAuthUrl(oauthConfig);
-        const response = await AuthSession.startAsync({ authUrl });
+        const response = await AuthSession.loadAsync({ authUrl });
 
         if (response.type === 'success') {
             // The accessToken is part of the response
