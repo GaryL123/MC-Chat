593146a7a8ae1bd0f7ab2cdb34be1b1cb4105551
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 14:39:13 -0400

    Update HomeHeader.js

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -21,7 +21,7 @@ export default function HomeHeader() {
         await logout();
     }
   return (
-    <View style={{paddingTop: ios? top:top+10 }} className="flex-row justify-between px-5 bg-indigo-400 pb-6 rounded-b-3xl shadow">
+    <View style={{paddingTop: ios? top:top+10 }} className="flex-row justify-between px-5 bg-green-700 pb-6 rounded-b-3xl shadow">
       <View>
         <Text style={{fontSize: hp(3)}} className="font-medium text-white">Chats</Text>
       </View>
