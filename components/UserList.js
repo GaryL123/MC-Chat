d88d3fbc4dc95764cb75d1196e49a7ae4bfc170e
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:05:10 -0400

    Create UserList.js

---
+++ components/UserList.js
@@ -0,0 +1,25 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import UserItem from './UserItem'
+import { useRouter } from 'expo-router'
+
+export default function UserList({ users, currentUser }) {
+  const router = useRouter();
+  return (
+    <View className="flex-1">
+      <FlatList
+        data={users}
+        contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
+        keyExtractor={item => Math.random()}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item, index }) => <UserItem
+          noBorder={index + 1 == users.length}
+          router={router}
+          currentUser={currentUser}
+          item={item}
+          index={index}
+        />}
+      />
+    </View>
+  )
+}
