13db5e1b1d0161e5afd354648b94aa2dccb0baea
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 22:15:41 -0400

    Update chatRoom.js

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -4,7 +4,7 @@ import { useLocalSearchParams, useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
 import ChatRoomHeader from '../../components/ChatRoomHeader';
 import MessageList from '../../components/MessageList';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Feather } from '@expo/vector-icons';
 import CustomKeyboardView from '../../components/CustomKeyboardView';
 import { useAuth } from '../../context/authContext';
@@ -14,14 +14,14 @@ import { db } from '../../firebaseConfig';
 
 export default function ChatRoom() {
     const item = useLocalSearchParams(); // second user
-    const {user} = useAuth(); // logged in user
+    const { user } = useAuth(); // logged in user
     const router = useRouter();
     const [messages, setMessages] = useState([]);
     const textRef = useRef('');
     const inputRef = useRef(null);
     const scrollViewRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         createRoomIfNotExists();
 
         let roomId = getRoomId(user?.userId, item?.userId);
@@ -29,8 +29,8 @@ export default function ChatRoom() {
         const messagesRef = collection(docRef, "messages");
         const q = query(messagesRef, orderBy('createdAt', 'asc'));
 
-        let unsub = onSnapshot(q, (snapshot)=>{
-            let allMessages = snapshot.docs.map(doc=>{
+        let unsub = onSnapshot(q, (snapshot) => {
+            let allMessages = snapshot.docs.map(doc => {
                 return doc.data();
             });
             setMessages([...allMessages]);
@@ -40,41 +40,41 @@ export default function ChatRoom() {
             'keyboardDidShow', updateScrollView
         )
 
-        return ()=>{
+        return () => {
             unsub();
             KeyboardDidShowListener.remove();
         }
 
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         updateScrollView();
-    },[messages])
+    }, [messages])
 
-    const updateScrollView = ()=>{
-        setTimeout(()=>{
-            scrollViewRef?.current?.scrollToEnd({animated: true})
-        },100)
+    const updateScrollView = () => {
+        setTimeout(() => {
+            scrollViewRef?.current?.scrollToEnd({ animated: true })
+        }, 100)
     }
 
-    const createRoomIfNotExists = async ()=>{
+    const createRoomIfNotExists = async () => {
         // roomId
         let roomId = getRoomId(user?.userId, item?.userId);
         await setDoc(doc(db, "rooms", roomId), {
-           roomId,
-           createdAt: Timestamp.fromDate(new Date()) 
+            roomId,
+            createdAt: Timestamp.fromDate(new Date())
         });
     }
 
-    const hanldeSendMessage = async ()=>{
+    const handleSendMessage = async () => {
         let message = textRef.current.trim();
-        if(!message) return;
-        try{
+        if (!message) return;
+        try {
             let roomId = getRoomId(user?.userId, item?.userId);
             const docRef = doc(db, 'rooms', roomId);
             const messagesRef = collection(docRef, "messages");
             textRef.current = "";
-            if(inputRef) inputRef?.current?.clear();
+            if (inputRef) inputRef?.current?.clear();
             const newDoc = await addDoc(messagesRef, {
                 userId: user?.userId,
                 text: message,
@@ -84,38 +84,38 @@ export default function ChatRoom() {
 
 
             // console.log('new message id: ', newDoc.id);
-        }catch(err){
+        } catch (err) {
             Alert.alert('Message', err.message);
         }
     }
 
-  return (
-    <CustomKeyboardView inChat={true}>
-        <View className="flex-1 bg-white">
-            <StatusBar style="dark" />
-            <ChatRoomHeader user={item} router={router} />
-            <View className="h-3 border-b border-neutral-300" />
-            <View className="flex-1 justify-between bg-neutral-100 overflow-visible">
-            <View className="flex-1">
-                <MessageList scrollViewRef={scrollViewRef} messages={messages} currentUser={user} />
-            </View>
-            <View style={{marginBottom: hp(2.7)}} className="pt-2">
-                <View className="flex-row mx-3 justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5">
-                    <TextInput 
-                        ref={inputRef}
-                        onChangeText={value=> textRef.current = value}
-                        placeholder='Type message...'
-                        placeholderTextColor={'gray'}
-                        style={{fontSize: hp(2)}}
-                        className="flex-1 mr-2"
-                    />
-                    <TouchableOpacity onPress={hanldeSendMessage} className="bg-neutral-200 p-2 mr-[1px] rounded-full">
-                        <Feather name="send" size={hp(2.7)} color="#737373" />
-                    </TouchableOpacity>
+    return (
+        <CustomKeyboardView inChat={true}>
+            <View className="flex-1 bg-white">
+                <StatusBar style="dark" />
+                <ChatRoomHeader user={item} router={router} />
+                <View className="h-3 border-b border-neutral-300" />
+                <View className="flex-1 justify-between bg-neutral-100 overflow-visible">
+                    <View className="flex-1">
+                        <MessageList scrollViewRef={scrollViewRef} messages={messages} currentUser={user} />
+                    </View>
+                    <View style={{ marginBottom: hp(2.7) }} className="pt-2">
+                        <View className="flex-row mx-3 justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5">
+                            <TextInput
+                                ref={inputRef}
+                                onChangeText={value => textRef.current = value}
+                                placeholder='Type message...'
+                                placeholderTextColor={'gray'}
+                                style={{ fontSize: hp(2) }}
+                                className="flex-1 mr-2"
+                            />
+                            <TouchableOpacity onPress={handleSendMessage} className="bg-neutral-200 p-2 mr-[1px] rounded-full">
+                                <Feather name="send" size={hp(2.7)} color="#737373" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
             </View>
-            </View>
-        </View>
-    </CustomKeyboardView>
-  )
+        </CustomKeyboardView>
+    )
 }
