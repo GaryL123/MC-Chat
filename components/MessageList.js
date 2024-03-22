7b343e81988535f1d5925fc241b314b0dd4199ca
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:03:29 -0400

    Update MessageList.js

--- components/MessageList.js
+++ components/MessageList.js
@@ -2,14 +2,14 @@ import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import MessageItem from './MessageItem'
 
-export default function MessageList({messages, scrollViewRef, currentUser}) {
+export default function MessageList({ messages, scrollViewRef, currentUser }) {
   return (
-    <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false} contentContainerStyle={{paddingTop: 10}}>
+    <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingTop: 10 }}>
       {
-        messages.map((message, index)=>{
-            return (
-                <MessageItem message={message} key={index} currentUser={currentUser} />
-            )
+        messages.map((message, index) => {
+          return (
+            <MessageItem message={message} key={index} currentUser={currentUser} />
+          )
         })
       }
     </ScrollView>
