ab6ffa51bed80f8e1db46fa9c0c8e673b45fbb5c
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:02:24 -0400

    Update CustomKeyboardView.js

--- components/CustomKeyboardView.js
+++ components/CustomKeyboardView.js
@@ -2,29 +2,29 @@ import { View, Text, KeyboardAvoidingView, ScrollView, Platform } from 'react-na
 import React from 'react'
 
 const ios = Platform.OS == 'ios';
-export default function CustomKeyboardView({children, inChat}) {
+export default function CustomKeyboardView({ children, inChat }) {
     let kavConfig = {};
     let scrollViewConfig = {};
-    if(inChat){
-        kavConfig = {keyboardVerticalOffset: 90};
-        scrollViewConfig = {contentContainerStyle: {flex: 1}};
+    if (inChat) {
+        kavConfig = { keyboardVerticalOffset: 90 };
+        scrollViewConfig = { contentContainerStyle: { flex: 1 } };
     }
-  return (
-    <KeyboardAvoidingView
-        behavior={ios? 'padding': 'height'}
-        style={{flex: 1}}
-        {...kavConfig}
-    >
-        <ScrollView
-            style={{flex: 1}}
-            bounces={false}
-            showsVerticalScrollIndicator={false}
-            {...scrollViewConfig}
+    return (
+        <KeyboardAvoidingView
+            behavior={ios ? 'padding' : 'height'}
+            style={{ flex: 1 }}
+            {...kavConfig}
         >
-            {
-                children
-            }
-        </ScrollView>
-    </KeyboardAvoidingView>
-  )
+            <ScrollView
+                style={{ flex: 1 }}
+                bounces={false}
+                showsVerticalScrollIndicator={false}
+                {...scrollViewConfig}
+            >
+                {
+                    children
+                }
+            </ScrollView>
+        </KeyboardAvoidingView>
+    )
 }
