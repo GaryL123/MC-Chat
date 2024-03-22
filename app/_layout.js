6b38a5519c963691c746bd3c33fee092fe51eecc
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 22:59:52 -0400

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,24 +1,24 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
-import {Slot, useRouter, useSegments} from "expo-router";
+import { Slot, useRouter, useSegments } from "expo-router";
 import "../global.css";
 import { AuthContextProvider, useAuth } from '../context/authContext';
 import { MenuProvider } from 'react-native-popup-menu';
 
-const MainLayout = ()=>{
-    const {isAuthenticated} = useAuth();
+const MainLayout = () => {
+    const { isAuthenticated } = useAuth();
     const segments = useSegments();
     const router = useRouter();
 
 
-    useEffect(()=>{
+    useEffect(() => {
         // check if user is authenticated or not
-        if(typeof isAuthenticated=='undefined') return;
-        const inApp = segments[0]=='(app)';
-        if(isAuthenticated && !inApp){
+        if (typeof isAuthenticated == 'undefined') return;
+        const inApp = segments[0] == '(app)';
+        if (isAuthenticated && !inApp) {
             // redirect to home
             router.push('home');
-        }else if(isAuthenticated==false){
+        } else if (isAuthenticated == false) {
             // redirect to signIn
             router.push('signIn');
         }
@@ -28,12 +28,11 @@ const MainLayout = ()=>{
 }
 
 export default function RootLayout() {
-  return (
-    <MenuProvider>
-        <AuthContextProvider>
-            <MainLayout />
-        </AuthContextProvider>
-    </MenuProvider>
-    
-  )
+    return (
+        <MenuProvider>
+            <AuthContextProvider>
+                <MainLayout />
+            </AuthContextProvider>
+        </MenuProvider>
+    )
 }
