765acae9f345dfb0851e9ca4506f558fe0cfa2a2
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-22 16:32:18 -0400

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,33 +1,39 @@
-import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/authContext'
 import { StatusBar } from 'expo-status-bar';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import ChatList from '../../components/ChatList';
-import Loading from '../../components/Loading';
-import { getDocs, query, where } from 'firebase/firestore';
+import { query, where, onSnapshot } from 'firebase/firestore';
 import { usersRef } from '../../firebaseConfig';
 
 export default function Home() {
   const { user } = useAuth();
   const [users, setUsers] = useState([]);
-  
-  useEffect(() => {
-    if (user?.uid)
-      getUsers();
-  }, [])
-  const getUsers = async () => {
-    // fetch users
-    const q = query(usersRef, where('userId', '!=', user?.uid));
 
-    const querySnapshot = await getDocs(q);
-    let data = [];
-    querySnapshot.forEach(doc => {
-      data.push({ ...doc.data() });
+  useEffect(() => {
+    if (user?.uid) {
+      console.log("home use effect if statement passed");
+      getUsers();
+    }
+  }, [])
+
+  const getUsers = () => {
+    console.log("home Getting users..."); // Confirm the function is called
+    const q = query(usersRef, where('uid', '!=', user?.uid));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      console.log("home Fetched users:", data);
+      setUsers(data);
+    }, (error) => {
+      console.error('home Error subscribing to users:', error);
     });
 
-    setUsers(data);
-  }
+    return unsubscribe;
+  };
 
   return (
     <View className="flex-1 bg-white">
@@ -38,7 +44,7 @@ export default function Home() {
           <ChatList currentUser={user} users={users} />
         ) : (
           <View className="flex items-center" style={{ top: hp(30) }}>
-            <Text>Welcome user</Text>
+            <Text>Add some friends!</Text>
             {/* <ActivityIndicator size="large" /> */}
             {/* <Loading size={hp(10)} /> */}
           </View>
