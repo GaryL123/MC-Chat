04db8e667082fca69147b629580dadc316f81785
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:12:02 -0400

    Update and rename settings to settings.js

--- app/(app)/settings.js
+++ app/(app)/settings.js
@@ -33,20 +33,20 @@ export default function Settings() {
 
   return (
     <View className="flex-1 bg-white">
-        <StatusBar style="light" />
-        <SettingsHeader router={router} />
-        {
-            users.length > 0 ? (
-                <UserList currentUser={user} users={users} />
-            ) : (
-                <View className="flex items-center" style={{ top: hp(30) }}>
-                    <Text>Welcome user</Text>
-                    {/* <ActivityIndicator size="large" /> */}
-                    {/* <Loading size={hp(10)} /> */}
-                </View>
-            )
-        }
+      <StatusBar style="light" />
+      <SettingsHeader router={router} />
+      {
+        users.length > 0 ? (
+          <UserList currentUser={user} users={users} />
+        ) : (
+          <View className="flex items-center" style={{ top: hp(30) }}>
+            <Text>Welcome user</Text>
+            {/* <ActivityIndicator size="large" /> */}
+            {/* <Loading size={hp(10)} /> */}
+          </View>
+        )
+      }
 
     </View>
-)
+  )
 }
