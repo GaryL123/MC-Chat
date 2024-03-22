712d3419b523dcb02386b08c1a369e25d88d58bb
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-22 15:27:11 -0400

    Update signUp.js

--- app/signUp.js
+++ app/signUp.js
@@ -14,19 +14,40 @@ export default function SignUp() {
     const [loading, setLoading] = useState(false);
 
     const emailRef = useRef("");
+    const fNameRef = useRef("");
+    const lNameRef = useRef("");
     const passwordRef = useRef("");
     const passwordConfirmRef = useRef("");
-    const usernameRef = useRef("");
-    const profileRef = useRef("");
+    const invalidChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
     const handleRegister = async () => {
-        if (!emailRef.current || !passwordRef.current) {
+        if (!emailRef.current || !fNameRef.current || !lNameRef.current || !passwordRef.current) {
             Alert.alert('Sign Up', "Please fill all the fields!");
             return;
         }
 
         if (emailRef.current.includes('@')) {
             Alert.alert('Sign Up', "Please enter only first half of email before @manhattan.edu");
+            return;
+        }
+
+        if(fNameRef.current.length < 2 || fNameRef.current.length > 30) {
+            Alert.alert('Sign Up', 'First Name should be between 2 and 30 characters')
+            return;
+        }
+
+        if(lNameRef.current.length < 2 || lNameRef.current.length > 30) {
+            Alert.alert('Sign Up', 'Last Name should be between 2 and 30 characters')
+            return;
+        }
+
+        if(invalidChars.test(fNameRef.current)) {
+            Alert.alert('Sign Up', 'First Name should not contain any special characters')
+            return;
+        }
+
+        if(invalidChars.test(lNameRef.current)) {
+            Alert.alert('Sign Up', 'Last Name should not contain any special characters')
             return;
         }
 
@@ -37,7 +58,10 @@ export default function SignUp() {
 
         setLoading(true);
 
-        let response = await register((emailRef.current + '@manhattan.edu'), passwordRef.current);
+        const capitalizationEmail = string => string.toLowerCase();
+        const capitalizationNames = string => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
+        let response = await register(capitalizationEmail(emailRef.current + '@manhattan.edu'), capitalizationNames(fNameRef.current), capitalizationNames(lNameRef.current), passwordRef.current);
         setLoading(false);
 
         console.log('got result: ', response);
@@ -48,17 +72,17 @@ export default function SignUp() {
     return (
         <CustomKeyboardView>
             <StatusBar style="dark" />
-            <View style={{ paddingTop: hp(10), paddingHorizontal: wp(5) }} className="flex-1 gap-12">
+            <View style={{ paddingTop: hp(10), paddingHorizontal: wp(5) }} className="flex-1 gap-7">
                 {/* signIn image */}
                 <View className="items-center">
-                    <Image style={{ height: hp(25) }} resizeMode='contain' source={require('../assets/images/MCChat_Color_512px.png')} />
+                    <Image style={{ height: hp(20) }} resizeMode='contain' source={require('../assets/images/MCChat_Color_512px.png')} />
                 </View>
 
 
-                <View className="gap-10">
+                <View className="gap-7">
                     <Text style={{ fontSize: hp(4) }} className="font-bold tracking-wider text-center text-neutral-800">Sign Up</Text>
                     {/* inputs */}
-                    <View className="gap-4">
+                    <View className="gap-3">
                         <View style={{ height: hp(7) }} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
                             <Octicons name="mail" size={hp(2.7)} color="gray" />
                             <TextInput
@@ -69,6 +93,26 @@ export default function SignUp() {
                                 placeholderTextColor={'gray'}
                             />
                             <Text style={{ fontSize: hp(2), color: '#6B7280' }}>@manhattan.edu</Text>
+                        </View>
+                        <View style={{ height: hp(7) }} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+                            <Octicons name="dot" size={hp(3.5)} color="gray" />
+                            <TextInput
+                                onChangeText={(value) => fNameRef.current = value}
+                                style={{ fontSize: hp(2) }}
+                                className="flex-1 font-semibold text-neutral-700"
+                                placeholder='First Name'
+                                placeholderTextColor={'gray'}
+                            />
+                        </View>
+                        <View style={{ height: hp(7) }} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+                            <Octicons name="dot" size={hp(3.5)} color="gray" />
+                            <TextInput
+                                onChangeText={(value) => lNameRef.current = value}
+                                style={{ fontSize: hp(2) }}
+                                className="flex-1 font-semibold text-neutral-700"
+                                placeholder='Last Name'
+                                placeholderTextColor={'gray'}
+                            />
                         </View>
                         <View style={{ height: hp(7) }} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
                             <Octicons name="lock" size={hp(2.7)} color="gray" />
