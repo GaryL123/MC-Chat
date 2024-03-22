3c2a85586765aa1a6583ba03acf1f3a05430b3ff
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-22 01:34:55 -0400

    Update ProfileHeader.js

--- components/ProfileHeader.js
+++ components/ProfileHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import { Entypo, Ionicons } from '@expo/vector-icons'
+import { Octicons } from '@expo/vector-icons'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Image } from 'expo-image';
 
@@ -14,7 +14,7 @@ export default function ProfileHeader({ router }) {
                 headerLeft: () => (
                     <View className="flex-row items-center gap-4">
                         <TouchableOpacity onPress={() => router.back()}>
-                            <Entypo name="chevron-left" size={hp(4)} color="#737373" />
+                            <Octicons name="chevron-left" size={hp(4)} color="#737373" />
                         </TouchableOpacity>
                         <View>
                             <Text style={{ fontSize: hp(2.5) }} className="text-neutral-700 font-medium">
