eb97c3661ae5db5aa2689a56252b1e336d9a15c7
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 22:15:52 -0400

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,49 +1,121 @@
-import { View, Text, Pressable, ActivityIndicator } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useAuth } from '../../context/authContext'
+import { View, Text, TextInput, TouchableOpacity, Alert, Keyboard } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import ChatList from '../../components/ChatList';
-import Loading from '../../components/Loading';
-import { getDocs, query, where } from 'firebase/firestore';
-import { usersRef } from '../../firebaseConfig';
+import ChatRoomHeader from '../../components/ChatRoomHeader';
+import MessageList from '../../components/MessageList';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { Feather } from '@expo/vector-icons';
+import CustomKeyboardView from '../../components/CustomKeyboardView';
+import { useAuth } from '../../context/authContext';
+import { getRoomId } from '../../utils/common';
+import { Timestamp, addDoc, collection, doc, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 
-export default function Home() {
-    const {logout, user} = useAuth();
-    const [users, setUsers] = useState([]);
-    useEffect(()=>{
-        if(user?.uid)
-            getUsers();
-    },[])
-    const getUsers = async ()=>{
-        // fetch users
-        const q = query(usersRef, where('userId', '!=', user?.uid));
+export default function ChatRoom() {
+    const item = useLocalSearchParams(); // second user
+    const { user } = useAuth(); // logged in user
+    const router = useRouter();
+    const [messages, setMessages] = useState([]);
+    const textRef = useRef('');
+    const inputRef = useRef(null);
+    const scrollViewRef = useRef(null);
 
-        const querySnapshot = await getDocs(q);
-        let data = [];
-        querySnapshot.forEach(doc=>{
-            data.push({...doc.data()});
+    useEffect(() => {
+        createRoomIfNotExists();
+
+        let roomId = getRoomId(user?.userId, item?.userId);
+        const docRef = doc(db, "rooms", roomId);
+        const messagesRef = collection(docRef, "messages");
+        const q = query(messagesRef, orderBy('createdAt', 'asc'));
+
+        let unsub = onSnapshot(q, (snapshot) => {
+            let allMessages = snapshot.docs.map(doc => {
+                return doc.data();
+            });
+            setMessages([...allMessages]);
         });
 
-        setUsers(data);
-    }
-    
-  return (
-    <View className="flex-1 bg-white">
-      <StatusBar style="light" />
-
-      {
-        users.length>0? (
-            <ChatList currentUser={user} users={users} />
-        ):(
-            <View className="flex items-center" style={{top: hp(30)}}>
-              <Text>Welcome user</Text>
-                {/* <ActivityIndicator size="large" /> */}
-                {/* <Loading size={hp(10)} /> */}
-            </View>
+        const KeyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow', updateScrollView
         )
-      }
-      
-    </View>
-  )
+
+        return () => {
+            unsub();
+            KeyboardDidShowListener.remove();
+        }
+
+    }, []);
+
+    useEffect(() => {
+        updateScrollView();
+    }, [messages])
+
+    const updateScrollView = () => {
+        setTimeout(() => {
+            scrollViewRef?.current?.scrollToEnd({ animated: true })
+        }, 100)
+    }
+
+    const createRoomIfNotExists = async () => {
+        // roomId
+        let roomId = getRoomId(user?.userId, item?.userId);
+        await setDoc(doc(db, "rooms", roomId), {
+            roomId,
+            createdAt: Timestamp.fromDate(new Date())
+        });
+    }
+
+    const handleSendMessage = async () => {
+        let message = textRef.current.trim();
+        if (!message) return;
+        try {
+            let roomId = getRoomId(user?.userId, item?.userId);
+            const docRef = doc(db, 'rooms', roomId);
+            const messagesRef = collection(docRef, "messages");
+            textRef.current = "";
+            if (inputRef) inputRef?.current?.clear();
+            const newDoc = await addDoc(messagesRef, {
+                userId: user?.userId,
+                text: message,
+                senderName: user?.email,
+                createdAt: Timestamp.fromDate(new Date())
+            });
+
+
+            // console.log('new message id: ', newDoc.id);
+        } catch (err) {
+            Alert.alert('Message', err.message);
+        }
+    }
+
+    return (
+        <CustomKeyboardView inChat={true}>
+            <View className="flex-1 bg-white">
+                <StatusBar style="dark" />
+                <ChatRoomHeader user={item} router={router} />
+                <View className="h-3 border-b border-neutral-300" />
+                <View className="flex-1 justify-between bg-neutral-100 overflow-visible">
+                    <View className="flex-1">
+                        <MessageList scrollViewRef={scrollViewRef} messages={messages} currentUser={user} />
+                    </View>
+                    <View style={{ marginBottom: hp(2.7) }} className="pt-2">
+                        <View className="flex-row mx-3 justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5">
+                            <TextInput
+                                ref={inputRef}
+                                onChangeText={value => textRef.current = value}
+                                placeholder='Type message...'
+                                placeholderTextColor={'gray'}
+                                style={{ fontSize: hp(2) }}
+                                className="flex-1 mr-2"
+                            />
+                            <TouchableOpacity onPress={handleSendMessage} className="bg-neutral-200 p-2 mr-[1px] rounded-full">
+                                <Feather name="send" size={hp(2.7)} color="#737373" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </CustomKeyboardView>
+    )
 }
