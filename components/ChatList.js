b7d0e6b712bc0f997f128ccec6aa8e45f3291f83
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:02:01 -0400

    Update ChatList.js

--- components/ChatList.js
+++ components/ChatList.js
@@ -3,23 +3,23 @@ import React from 'react'
 import ChatItem from './ChatItem'
 import { useRouter } from 'expo-router'
 
-export default function ChatList({users, currentUser}) {
-    const router = useRouter();
+export default function ChatList({ users, currentUser }) {
+  const router = useRouter();
   return (
     <View className="flex-1">
       <FlatList
         data={users}
-        contentContainerStyle={{flex: 1, paddingVertical: 25}}
-        keyExtractor={item=> Math.random()}
+        contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
+        keyExtractor={item => Math.random()}
         showsVerticalScrollIndicator={false}
-        renderItem={({item, index})=> <ChatItem 
-            noBorder={index+1 == users.length} 
-            router={router} 
-            currentUser={currentUser}
-            item={item} 
-            index={index} 
+        renderItem={({ item, index }) => <ChatItem
+          noBorder={index + 1 == users.length}
+          router={router}
+          currentUser={currentUser}
+          item={item}
+          index={index}
         />}
-       />
+      />
     </View>
   )
 }
