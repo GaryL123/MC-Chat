6ae27e568658207614fe128b89b481354e7f432e
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 23:03:05 -0400

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -2,10 +2,10 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import LottieView from 'lottie-react-native';
 
-export default function Loading({size}) {
+export default function Loading({ size }) {
   return (
-    <View style={{height: size, aspectRatio: 1}}>
-      <LottieView style={{flex: 1}} source={require('../assets/images/loading.json')} autoPlay loop />
+    <View style={{ height: size, aspectRatio: 1 }}>
+      <LottieView style={{ flex: 1 }} source={require('../assets/images/loading.json')} autoPlay loop />
     </View>
   )
 }
