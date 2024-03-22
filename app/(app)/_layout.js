bff89a2ce8f86178e69509d13f94f36a226470c1
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-21 22:15:29 -0400

    Update _layout.js

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -6,13 +6,12 @@ import HomeHeader from '../../components/HomeHeader'
 export default function _layout() {
   return (
     <Stack>
-        <Stack.Screen
-            name="home"
-            options={{
-                header: ()=> <HomeHeader />
-            }}
-        />
-
+      <Stack.Screen
+        name="home"
+        options={{
+          header: () => <HomeHeader />
+        }}
+      />
     </Stack>
   )
 }
