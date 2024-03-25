e81b0f042a18d60fb9f56d551acd6e7c48eff4f3
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-03-25 17:44:55 -0400

    Update CustomMenuItems.js

--- components/CustomMenuItems.js
+++ components/CustomMenuItems.js
@@ -1,16 +1,17 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import { MenuOption } from 'react-native-popup-menu';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-export const MenuItem = ({ text, action, value, icon }) => {
+export const MenuItem = ({ text, action, value, icon, customContent }) => {
     return (
         <MenuOption onSelect={() => action(value)}>
             <View className="px-4 py-1 flex-row justify-between items-center">
                 <Text style={{ fontSize: hp(1.7) }} className="font-semibold text-neutral-600">
                     {text}
                 </Text>
-                {icon}
+                {customContent ? customContent : icon}
             </View>
         </MenuOption>
-    )
-}
+    );
+};
