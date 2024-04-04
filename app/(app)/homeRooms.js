6e5abaea02efbdcec61eef7bb0d6fb0deb846678
GaryL123 <126824940+GaryL123@users.noreply.github.com>
2024-04-04 17:37:58 -0400

    Update homeRooms.js

--- app/(app)/homeRooms.js
+++ app/(app)/homeRooms.js
@@ -3,57 +3,35 @@ import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/authContext'
 import { StatusBar } from 'expo-status-bar';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import ChatList from '../../components/ChatList';
+import RoomList from '../../components/RoomItemList';
 import { collection, doc, getDocs, getDoc, query, where, onSnapshot } from 'firebase/firestore';
 import { db, usersRef } from '../../firebaseConfig';
 
-export default function Home() {
+export default function HomeRooms() {
   const { user } = useAuth();
-  const [users, setUsers] = useState([]);
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
     console.log("home use effect started");
-    if (user?.uid) {
-      console.log("home use effect if statement passed");
-      getRooms();
-    }
-  }, [user?.uid])
-
-  /*const getUsers = async () => {
-    console.log("home Getting users..."); // Confirm the function is called
-    const q = query(usersRef, where('uid', '!=', user?.uid));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let data = [];
-      querySnapshot.forEach((doc) => {
-        data.push({ ...doc.data(), id: doc.id });
-      });
-      console.log("home Fetched users:", data);
-      setUsers(data);
-    }, (error) => {
-      console.error('home Error subscribing to users:', error);
-    });
-
-    return unsubscribe;
-  };*/
+    getRooms();
+    
+  })
 
   const getRooms = () => {
-    if (!user?.uid) return; // Guard clause if user is not defined
-
     const roomsRef = collection(db,'chatRooms');
-    onSnapshot(roomsRef, async (roomsSnapshot) => {
-        let friendPromises = roomsSnapshot.docs.map(docSnap => {
-            const friendUid = docSnap.id;
-            return getDoc(doc(db, 'users', friendUid));
+    onSnapshot(roomsRef, async (roomSnapshot) => {
+        let roomPromises = roomSnapshot.docs.map(docSnap => {
+            const roomName = docSnap.name;
+            return getDoc(doc(db, 'chatRooms', roomName));
         });
 
-        const friendDocs = await Promise.all(friendPromises);
-        const roomsData = friendDocs.filter(docSnap => docSnap.exists()).map(docSnap => ({
+        const roomDocs = await Promise.all(roomPromises);
+        const roomData = roomDocs.filter(docSnap => docSnap.exists()).map(docSnap => ({
             id: docSnap.id,
             ...docSnap.data()
         }));
 
-        setRooms(roomsData); // Update the rooms state
+        setRooms(roomData); // Update the rooms state
     });
 };
 
@@ -63,7 +41,7 @@ export default function Home() {
 
       {
         rooms.length > 0 ? (
-          <ChatList currentUser={user} rooms={rooms} />
+          <RoomList currentUser={user} rooms={rooms} />
         ) : (
           <View className="flex items-center" style={{ top: hp(30) }}>
             <Text>Add some rooms!</Text>
