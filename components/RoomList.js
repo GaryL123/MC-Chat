57ecf3522002032dc88d7a503da90b19fade4aca
GaryL123 <126824940+GaryL123@users.noreply.github.com>
2024-04-04 18:46:57 -0400

    Update RoomList.js

--- components/RoomList.js
+++ components/RoomList.js
@@ -1,25 +1,26 @@
 import { View, Text, FlatList } from 'react-native'
 import React from 'react'
-import ChatItem from './ChatItem'
+import RoomItem from './RoomItem'
 import { useRouter } from 'expo-router'
 
-export default function RoomList({ users, currentUser }) {
+export default function RoomList({ rooms }) {
   const router = useRouter();
   return (
     <View className="flex-1">
       <FlatList
-        data={users}
+        data={rooms}
         contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
         keyExtractor={item => Math.random()}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item, index }) => <ChatItem
-          noBorder={index + 1 == users.length}
+        renderItem={({ item, index }) =>  
+        <RoomItem
+          noBorder={index + 1 == rooms.length}
           router={router}
           currentUser={currentUser}
           item={item}
           index={index}
         />}
-      />
+      /> 
     </View>
   )
 }
