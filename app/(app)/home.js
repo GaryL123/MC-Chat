205fda8067bd0b0647eabfb203ee6197263d4e0e
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-04-04 13:26:57 -0400

    Update home.js

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -4,22 +4,23 @@ import { useAuth } from '../../context/authContext'
 import { StatusBar } from 'expo-status-bar';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import ChatList from '../../components/ChatList';
-import { query, where, onSnapshot } from 'firebase/firestore';
-import { usersRef } from '../../firebaseConfig';
+import { collection, doc, getDocs, getDoc, query, where, onSnapshot } from 'firebase/firestore';
+import { db, usersRef } from '../../firebaseConfig';
 
 export default function Home() {
   const { user } = useAuth();
   const [users, setUsers] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   useEffect(() => {
     console.log("home use effect started");
     if (user?.uid) {
       console.log("home use effect if statement passed");
-      getUsers();
+      getFriends();
     }
   }, [user?.uid])
 
-  const getUsers = async () => {
+  /*const getUsers = async () => {
     console.log("home Getting users..."); // Confirm the function is called
     const q = query(usersRef, where('uid', '!=', user?.uid));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -34,15 +35,34 @@ export default function Home() {
     });
 
     return unsubscribe;
-  };
+  };*/
+
+  const getFriends = async () => {
+    const friendsRef = collection(db, 'users', user.uid, 'friends');
+    const friendsSnapshot = await getDocs(friendsRef);
+
+    let friendPromises = friendsSnapshot.docs.map(docSnap => {
+        const friendUid = docSnap.id;
+        return getDoc(doc(db, 'users', friendUid));
+    });
+
+    const friendDocs = await Promise.all(friendPromises);
+    const friendsData = friendDocs.filter(docSnap => docSnap.exists()).map(docSnap => ({
+        id: docSnap.id,
+        ...docSnap.data()
+    }));
+
+    setFriends(friendsData); // Use setFriends to store the fetched friend data
+};
+
 
   return (
     <View className="flex-1 bg-white">
       <StatusBar style="light" />
 
       {
-        users.length > 0 ? (
-          <ChatList currentUser={user} users={users} />
+        friends.length > 0 ? (
+          <ChatList currentUser={user} friends={friends} />
         ) : (
           <View className="flex items-center" style={{ top: hp(30) }}>
             <Text>Add some friends!</Text>
