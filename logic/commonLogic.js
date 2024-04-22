ab6e5cee20b3c0238eba4e2cfa2782743c427975
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-04-22 13:37:02 -0400

    Update commonLogic.js

--- logic/commonLogic.js
+++ logic/commonLogic.js
@@ -1,5 +1,7 @@
 export const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
+export const defaultProfilePicture = "https://firebasestorage.googleapis.com/v0/b/mc-chat-b6bef.appspot.com/o/profilePictures%2Fdefault.jpeg?alt=media&token=7ea7f74c-faab-476a-bff6-84746602095c";
+
 export const getChatId = (userId1, userId2) => {
     const sortedIds = [userId1, userId2].sort();
     const chatId = sortedIds.join('-');
