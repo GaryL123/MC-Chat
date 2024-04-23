458088d0213a31cb0244b4eb612179159628c2b6
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2024-04-23 09:20:17 -0400

    Create RoomsSettingsScreen.js

---
+++ screens/RoomsSettingsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function RoomsSettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to Home!</Text>
+    </View>
+  );
+}
