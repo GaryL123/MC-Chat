2497662d3e7d086a60c577b7e1c2e2227ceee63e
tscar-1 <thomas.a.scardino@gmail.com>
2024-04-23 20:37:35 -0400

    Fixed header options import

--- navigators/FriendRequestsStackNavigator.js
+++ navigators/FriendRequestsStackNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import FriendRequestsScreen from '../screens/FriendRequestsScreen';
-import HeaderScreenOptions from './navigationConfig';
+import { HeaderScreenOptions } from './navigationConfig';
 
 const FriendRequestsStack = createStackNavigator();
 
--- navigators/ProfileStackNavigator.js
+++ navigators/ProfileStackNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
-import HeaderScreenOptions from './navigationConfig';
+import { HeaderScreenOptions } from './navigationConfig';
 
 const ProfileStack = createStackNavigator();
 
