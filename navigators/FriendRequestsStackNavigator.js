acf1d2fa0a556f8bca2c95499e03a9373c35ff33
tscar-1 <thomas.a.scardino@gmail.com>
2024-04-25 02:37:42 -0400

    more translations

--- navigators/FriendRequestsStackNavigator.js
+++ navigators/FriendRequestsStackNavigator.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import FriendRequestsScreen from '../screens/FriendRequestsScreen';
 import { HeaderScreenOptions } from './navigationConfig';
+import translations from '../assets/styles/Translations';
+import { useSettings } from '../logic/settingsContext';
 
 const FriendRequestsStack = createStackNavigator();
 
-const FriendRequestsStackNavigator = () => (
-  <FriendRequestsStack.Navigator screenOptions={HeaderScreenOptions}>
-    <FriendRequestsStack.Screen name="Friend Requests" component={FriendRequestsScreen} />
-  </FriendRequestsStack.Navigator>
-);
+const FriendRequestsStackNavigator = () => {
+  const t = (key) => translations[key][language] || translations[key]['English'];
+  const { language } = useSettings();
+
+  return (
+    <FriendRequestsStack.Navigator screenOptions={HeaderScreenOptions}>
+      <FriendRequestsStack.Screen name={t("Friend Requests")} component={FriendRequestsScreen} />
+    </FriendRequestsStack.Navigator>
+  );
+};
 
 export default FriendRequestsStackNavigator;
--- navigators/RoomInvitesStackNavigator.js
+++ navigators/RoomInvitesStackNavigator.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import RoomsInvitesScreen from '../screens/RoomsInvitesScreen';
 import { HeaderScreenOptions } from './navigationConfig';
+import translations from '../assets/styles/Translations';
+import { useSettings } from '../logic/settingsContext';
 
 const RoomInvitesStack = createStackNavigator();
 
-const RoomInvitesStackNavigator = () => (
-  <RoomInvitesStack.Navigator screenOptions={HeaderScreenOptions}>
-    <RoomInvitesStack.Screen name="Room Invites" component={RoomsInvitesScreen} />
-  </RoomInvitesStack.Navigator>
-);
+const RoomInvitesStackNavigator = () => {
+  const t = (key) => translations[key][language] || translations[key]['English'];
+  const { language } = useSettings();
+
+  return (
+    <RoomInvitesStack.Navigator screenOptions={HeaderScreenOptions}>
+      <RoomInvitesStack.Screen name={t("Room Invites")} component={RoomsInvitesScreen} />
+    </RoomInvitesStack.Navigator>
+  );
+};
 
 export default RoomInvitesStackNavigator;
