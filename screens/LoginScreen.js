57467dc6db7e33808d66c3435ab01f1ebdf1ca99
LouieCeliberti <Louismceliberti@gmail.com>
2024-05-02 11:34:52 -0400

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, Text, TouchableOpacity, Image, StyleSheet, Alert, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Octicons } from '@expo/vector-icons';
@@ -10,14 +10,29 @@ import translations from '../assets/styles/Translations';
 import styles from '../assets/styles/AppStyles';
 import { getldStyles } from '../assets/styles/LightDarkStyles';
 
+// Constants for account lockout and timer management
+const MAX_LOGIN_ATTEMPTS = 5; // Maximum number of failed login attempts before lockout
+const LOCKOUT_DURATION = 60 * 60 * 1000; // Lockout duration in milliseconds (60 minutes)
+
 function LoginScreen() {
   const { language, darkMode, profanityFilter, textSize } = useSettings();
   const navigation = useNavigation();
   const [emailPrefix, setEmailPrefix] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [loginAttempts, setLoginAttempts] = useState(0); // State to track login attempts
+  const [lockoutTime, setLockoutTime] = useState(null); // State to track lockout duration
   const t = (key) => translations[key][language] || translations[key]['English'];
   const ldStyles = getldStyles(textSize);
+
+  useEffect(() => {
+    if (lockoutTime) {
+      const timerId = setTimeout(() => {
+        setLockoutTime(null);
+      }, lockoutTime - Date.now());
+      return () => clearTimeout(timerId);
+    }
+  }, [lockoutTime]);
 
   const handleLogin = async () => {
     if (!emailPrefix || !password) {
@@ -35,11 +50,26 @@ function LoginScreen() {
     setLoading(true);
     try {
       const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+      setLoginAttempts(0);
       setLoading(false);
     } catch (error) {
       setLoading(false);
-      Alert.alert('Login Error', error.message);
+      if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
+        Alert.alert('Login Error', 'Invalid email or password.');
+      } else {
+        setLoginAttempts(loginAttempts + 1);
+        if (loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS) {
+          await lockAccount();
+          return;
+        }
+        Alert.alert('Login Error', error.message);
+      }
     }
+  };
+
+  const lockAccount = async () => {
+    Alert.alert('Account Locked', 'Your account has been locked due to multiple failed login attempts.');
+    setLockoutTime(Date.now() + LOCKOUT_DURATION);
   };
 
   return (
@@ -86,13 +116,21 @@ function LoginScreen() {
               <Text style={styles.forgotPasswordText}>{t("Forgot Password")}?</Text>
             </TouchableOpacity>
 
-            {loading ? (
+            {!lockoutTime && ( 
+              loading ? (
+                <View style={styles.centered}>
+                </View>
+              ) : (
+                <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
+                  <Text style={styles.loginButtonText}>{t("Sign In")}</Text>
+                </TouchableOpacity>
+              )
+            )}
+
+            {lockoutTime && ( 
               <View style={styles.centered}>
+                <Text style={[darkMode? ldStyles.nameTextD : ldStyles.nameTextL]}>Your account is locked. Please try again later.</Text>
               </View>
-            ) : (
-              <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
-                <Text style={styles.loginButtonText}>{t("Sign In")}</Text>
-              </TouchableOpacity>
             )}
 
             <View style={styles.registerContainer}>
